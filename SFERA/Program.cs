ea0e89d23e8fc32582f74ed4f94713f10bb56088
USER <USER@PC-0>
2023-10-16 10:52:16 +0300

    Add project files.

---
+++ SFERA/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+using System.Text;
+namespace SFERA
+{
+    internal class Program
+    {
+        static double Surface(double X)
+        {
+            double Z = 4 * Math.PI * X * X;
+            return Z;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Write("Въведете радиуса на сферата R = ");
+            float R = float.Parse(ReadLine());
+            double V = Surface(R) * R / 3.0;
+            double S = Surface(R);
+            WriteLine($"Обема на сферата е {V} куб. метра");
+            WriteLine($"Лицето на сферата е {S} кв. метра");
+        }
+    }
+}
